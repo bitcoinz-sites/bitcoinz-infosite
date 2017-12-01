44ba81b7744af2a83f74ec05059880ca75be13b7
cryptorex <hungryhodlr@scryptmail.com>
2017-12-01 09:57:42 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bitcoinz-infosite
+
+git clone https://github.com/btcz/bitcoinz-infosite.git
+npm install
+npm start
---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var https = require('https');
+var fs = require('fs');
+
+// Self-signed SSL certs
+// created with 'openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout info.btcz.rocks.key -out info.btcz.rocks.crt'
+
+var key = fs.readFileSync('certs/info.btcz.rocks.key');
+var cert = fs.readFileSync('certs/info.btcz.rocks.crt');
+
+var options = {
+  key: key,
+  cert: cert
+};
+
+
+app.use(express.static(__dirname + '/public'));
+//Store all HTML files in public folder.
+
+app.use(express.static(__dirname + '/public/scripts'));
+//Store all JS and /public/scripts folder.
+
+app.use(express.static(__dirname + '/public/css'));
+//Store all CSS in css folder
+
+
+app.use(express.static(__dirname + '/public/images'));
+//Store all IMAGES in images folder
+
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from public
+});
+
+https.createServer(options, app).listen(3001);
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000')
+});
