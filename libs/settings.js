5823c89a7f59c32dceb678348d6b490945c6d1b8
cryptorex <hungryhodlr@scryptmail.com>
2018-03-02 16:09:56 -0600

    nesting JSON work in progress

--- libs/settings.js
+++ libs/settings.js
@@ -2,29 +2,44 @@ var settings = {};
 
 settings.title = "BitcoinZ Progress";
 
-
 settings.panels = {
-	"proposals": {"title": "Proposals", 
-		      "headings": {
-				"numb": "#",
-				"type": "Type",
-				"name": "Name",
-				"budget": "Budget",
-				"status": "Status",
-				"links": "Links"
-				}                 
-		             },
-	
-	"help_wanted": {"title": "Help Wanted",
-			"headings": {
-				 "numb": "#",
-                                 "type": "Type",
-				 "description": "Description",
-				 "web_link": "Web Link",
-                                 "repository": "Repository"
+	"proposals": {
+		"title": "Proposals",
+		"headings": {
+			"numb": "#",
+			"type": "Type",
+			"name": "Name",
+			"budget": "Budget",
+			"status": "Status",
+			"links": "Links"
+		}, 
+		"info": {
+			"numb1": "2",
+			"type1": "Community Campaign",
+			"name1": "Community YouTube / Twitter Campaign #1",
+			"budget1": "100,000 BTCZ per week / one week duration",
+			"status1": "Vote Passed / Campaign Expired",
+			"links1": {
+				"link1": "http://www.strawpoll.me/14517768/r",
+				"link2": "https://docs.google.com/document/d/1fnsY7khYimf9wOXcj6-hO0tDHVXYeFVbcql_lP3ofK4 edit?usp=sharing"
 				}
-	                     }
-	
-			};
+                            
+			}
+		
+	},
+
+
+	"help_wanted": {
+		"title": "Help Wanted",
+		"headings": {
+			"numb": "#",
+			"type": "Type",
+			"description": "Description",
+			"web_link": "Web Link",
+			"repository": "Repository"
+		}
+	}
+
+};
 
 module.exports = settings;
