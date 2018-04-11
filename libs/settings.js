ca7e45330694bd3b3ed3e510b9155f0c11e52227
cryptorex <hungryhodlr@scryptmail.com>
2018-04-11 16:46:45 -0500

    integrated arrays for multi-link with labels; wrapping links in html

--- libs/settings.js
+++ libs/settings.js
@@ -15,27 +15,39 @@ settings.panels = {
 		}, 
 		"info": [ 
 		        {
-    		        "numb": "5",
+    		        "numb": "",
 			"type": "Community Campaign",
 			"name": "Community YouTube / Twitter Campaign #1",
 			"budget": "100,000 BTCZ per week / one week duration",
 			"status": "Vote Passed / Campaign Expired",
 			"links": {
-				"link1": "http://www.strawpoll.me/14517768/r",
-				"link2": "https://docs.google.com/document/d/1fnsY7khYimf9wOXcj6-hO0tDHVXYeFVbcql_lP3ofK4 edit?usp=sharing"
+				"link1": ["Results", "http://www.strawpoll.me/14517768/r"],
+				"link2": ["Payment Updated", "https://docs.google.com/document/d/1fnsY7khYimf9wOXcj6-hO0tDHVXYeFVbcql_lP3ofK4/edit?usp=sharing"]
 				}
-			  },
-		         {          
-			"numb": "3",
+			},
+		        {          
+			"numb": "",
 			"type": "Community Competition",
 			"name": "Community Competition #1",
 			"budget": "45,000 - 75,000 BTCZ",
 			"status": "Vote Passed",
 			"links": {
-				"link1": "http://www.strawpoll.me/14518351/r",
-				"link2": "https://docs.google.com/document/d/14mPlKiQyef8rmTqZnY0hdKBHANtTEEbWXAgZY50ORKQ/edit?usp=sharing"
+				"link1": ["Results", "http://www.strawpoll.me/14518351/r"],
+				"link2": ["Details", "https://docs.google.com/document/d/14mPlKiQyef8rmTqZnY0hdKBHANtTEEbWXAgZY50ORKQ/edit?usp=sharing"]
 				}
-              }]
+                        },
+			{
+			"numb": "",
+			"type": "PR & Media",
+			"name": "PR & Media Campaign #1",
+			"budget": "150,000 BTCZ +25,000 BTCZ to add new country",
+			"status": "Vote Passed / Distributing ",
+			"links": {
+				"link1": ["Results", "http://www.strawpoll.me/14564234/r"],
+				"link2": ["Payment Details", "https://docs.google.com/document/d/1TDjSG-d7u2oWrba9ZMxfz_F3CADq7n53Sj_tGPAM8Ss/edit?usp=sharing"]
+			        }				
+			}
+		      ]
 		   },
 	"help_wanted": {
 		"title": "Help Wanted",
@@ -48,19 +60,23 @@ settings.panels = {
 		},
 		"info": [
 			{
-			"numb": "2",
+			"numb": "",
 			"type": "Website",
 			"description": "Primary Website",
 			"web_link": "https://btcz.rocks",
-			"repository": "https://github.com/bitcoinz-sites/btcz.rocks"
+			"links": {
+               	            "link1": ["GitHub", "https://github.com/bitcoinz-sites/btcz.rocks"]
+                           }
 			},
 			{ 
-			"numb": "1",
+			"numb": "",
 			"type": "Website",
 			"description": "Progress / Info Bulletin",
 			"web_link": "https://info.btcz.rocks/",
-			"repository": "https://github.com/bitcoinz-sites/bitcoinz-infosite"
-            }
+			"links": {
+     			    "link1": ["GitHub", "https://github.com/bitcoinz-sites/bitcoinz-infosite"]
+                           }
+                }
 	    ]}
     };
 
