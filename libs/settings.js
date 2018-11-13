13206e00a8dbdf01dee18a2aafa0610cf8a4374b
cryptorex <hungryhodlr@scryptmail.com>
2018-11-13 10:45:46 -0700

    update yobit information

--- libs/settings.js
+++ libs/settings.js
@@ -1027,6 +1027,18 @@ settings.panels = {
                	            "link1": ["Trade Page", "https://tradesatoshi.com/Exchange?market=BTCZ_BTC"]
                            }
 			},
+                        {
+                        "numb": "",
+                        "isnew": "true",
+                        "highlight": "success", // danger = red; success = green;
+                        "name": "Yobit.net",
+                        "cost": "0.20 BTC (0.06 BTC paid from community funds)",
+                        "status": "Active",
+                        "links": {
+                            "link1": ["Trade Page - Pending", ""],
+                            "link2": ["Payment Details", "https://docs.google.com/document/d/1gdVwfvkvm96WNKVxci-nJtvvn68wC8oLbu6Qw8hIW1Q/edit?usp=sharing"]
+                           }
+                        },
 			{
 			"numb": "",
                         "isnew": "false",
